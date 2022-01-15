6dbd6a386ac66cee39dc5482d5df08ca00b0a830
gizemdemirtaass <79507052+gizemdemirtaass@users.noreply.github.com>
2022-01-15 15:07:11 +0300

    Add files via upload

---
+++ TamSayıAlma/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz : ");
+        int a = scan.nextInt();
+        System.out.println("Girilen sayı: "+ a);
+    }
+}
